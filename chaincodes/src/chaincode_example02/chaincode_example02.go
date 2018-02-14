b1c0ae0e7739a509995481d4eac6d21f7fe41957
Mt <mtnietotcpsi@gmail.com>
2018-02-14 17:01:05 +0100

    Registrar is not working and I dont know why :)

--- chaincodes/src/chaincode_example02/chaincode_example02.go
+++ chaincodes/src/chaincode_example02/chaincode_example02.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
@@ -19,6 +18,8 @@ type Code struct {
 	Target  string
 }
 
+var targetList []string
+
 // Init to initiate the SimpleChaincode class
 func (t *ManagementChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	logger.Debug("[Management Chaincode][Init]Instanciating chaincode...")
@@ -27,45 +28,93 @@ func (t *ManagementChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response
 
 // Invoke a method specified in the SimpleChaincode class
 func (t *ManagementChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	logger.Debug("[Management Chaincode][Invoke]Invoking chaincode...")
+	logger.Critical("[Management Chaincode][Invoke]Invoking chaincode...")
 	function, args := stub.GetFunctionAndParameters()
 	if function == "storeCode" {
 		return t.storeCode(stub, args)
 	}
+	if function == "registrar" {
+		return t.registrar(stub, args)
+	}
 	return shim.Success([]byte("Invoke"))
+}
+func (t *ManagementChaincode) registrar(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	logger.Critical("[Management Chaincode][Registrar]Calling registrar...")
+	//Get the transaction ID
+	txID := stub.GetTxID()
+	logger.Debug("[Management Chaincode][StoreCode]Transaction ID", txID)
+	// Get certs from transaction sender
+	caller, err := stub.GetCreator()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	//Get alias to store
+	alias := args[0]
+	logger.Critical("[Management Chaincode][StoreCode]Storing cert, alias", args[0])
+	err = stub.PutState(string(caller[:]), []byte(alias))
+	if err != nil {
+		logger.Error("[Management Chaincode][addTarget]Problem adding new target..", err)
+		return shim.Error(err.Error())
+	}
+	logger.Critical("[Management Chaincode][StoreCode]Stored successful", args[0])
+	state, err := stub.GetState(string(caller[:]))
+	if err != nil {
+		logger.Error("[Management Chaincode][addTarget]Problem adding new target..", err)
+		return shim.Error(err.Error())
+	}
+	logger.Critical("[Management Chaincode][StoreCode]State", string(state[:]))
+	return shim.Success([]byte(caller))
 }
 
 func (t *ManagementChaincode) storeCode(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	logger.Debug("[Management Chaincode][StoreCode]Calling storeCode...")
+	logger.Critical("[Management Chaincode][StoreCode]Calling storeCode...")
 	//Get the transaction ID
 	txID := stub.GetTxID()
-	logger.Debug("[Management Chaincode][StoreCode]Transaction ID", txID)
+	logger.Critical("[Management Chaincode][StoreCode]Transaction ID", txID)
 	caller, err := stub.GetCreator()
 
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	rawIn := json.RawMessage(args[1])
-	bytes, err := rawIn.MarshalJSON()
-	if err != nil {
-		panic(err)
-	}
-	var something Code
-	err = json.Unmarshal(bytes, &something)
-	fmt.Println(something)
-	callerStr := string(caller[:])
-	chaincodeName := args[0]
-	something.Target = callerStr
-	fmt.Println("The caller", callerStr)
-	var data []byte
+	// rawIn := json.RawMessage(args[1])
+	// bytes, err := rawIn.MarshalJSON()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// var something Code
+	// err = json.Unmarshal(bytes, &something)
+	// callerStr := string(caller[:])
+	// chaincodeName := args[0]
+	// something.Target = callerStr
+	// logger.Critical("The caller", callerStr)
+	// var data []byte
 
-	data = []byte(args[1])
-	stub.PutState(chaincodeName, data)
+	// data = []byte(args[1])
+	// stub.PutState(chaincodeName, data)
 	return shim.Success([]byte(caller))
 }
+func addTarget(stub shim.ChaincodeStubInterface, newTarget string) bool {
+	state, err := stub.GetState("targetList")
+	if err != nil {
+		logger.Error("[Management Chaincode][addTarget]Problem adding new target..", err)
+		return false
+	}
+	json.Unmarshal(state, &targetList)
+	slice := append(targetList, newTarget)
+	logger.Critical("[Management Chaincode][addTarget] Actual state of the slice ..", slice)
+	toStore, err := json.Marshal(slice)
+	if err != nil {
+		return false
+	}
+	logger.Critical("[Management Chaincode][addTarget] Updating the state...")
+	stub.PutState("targetList", toStore)
+	logger.Critical("[Management Chaincode][addTarget] Updated the state")
 
+	return true
+}
 func main() {
 	err := shim.Start(new(ManagementChaincode))
 	if err != nil {
